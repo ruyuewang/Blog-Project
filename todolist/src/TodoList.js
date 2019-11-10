6b73b52cdb523a7107e33a54f90d17ff65933c34
Yufei Gao <ygao6@wpi.edu>
2019-11-10 17:08:07 -0500

    state

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,71 @@
+import React, {Component, Fragment} from 'react';
+
+// import {Component} from 'react';
+// 等价于
+// import React from 'react';
+// const Component = react.Component
+
+
+class TodoList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+    }
+    render(){
+        return (
+            <Fragment>
+                <div>
+                    <input
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange.bind(this)}
+                    />
+                    <button>add</button>
+                </div>
+                <ul>
+                    <li>Movie</li>
+                    <li>Blog</li>
+                </ul>
+            </Fragment>
+        )
+    }
+
+    handleInputChange(e){
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+}
+
+/*
+function TodoList() {
+    // JSX 语法：在js中使用<标签>不需要引号
+    // 如果要使用自己创造的组件 可以写成标签<APP />
+    // react组件标签 大写字母开头
+    // h5标签 小写字母开头
+  return (
+    <Fragment>
+        <div><input/><button>add</button></div>
+        <ul>
+            <li>Movie</li>
+            <li>Blog</li>
+        </ul>
+    </Fragment>
+  );
+}
+**/
+/* ES6写法 这个class和上面的function是等效的
+// 当一个class extends React.Component，这个class就会变成react组件
+// render方法返回的内容就是这个组件显示的内容
+class TodoList extends React.Component{
+    render(){
+        return(
+        <div>
+            TodoList
+        </div>);
+    }
+}
+*/
+export default TodoList;
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,7 +1,7 @@
 // 使用了 JSX 语法必须引用react
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TodoList from './TodoList';
 // 把App组件挂载到id=root这个DOM上
 // JSX 语法
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
