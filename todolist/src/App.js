60666f80f148907a16d14738ad9353d2e8714e9d
Yufei Gao <ygao6@wpi.edu>
2019-11-10 16:02:35 -0500

    react组件

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+// import {Component} from 'react';
+// 等价于
+// import React from 'react';
+// const Component = react.Component
+
 function App() {
   return (
     <div>
@@ -8,4 +13,16 @@ function App() {
   );
 }
 
+/* ES6写法 这个class和上面的function是等效的
+// 当一个class extends React.Component，这个class就会变成react组件
+// render方法返回的内容就是这个组件显示的内容
+class App extends React.Component{
+    render(){
+        return(
+        <div>
+            TodoList
+        </div>);
+    }
+}
+*/
 export default App;
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,4 +1,7 @@
+// 使用了 JSX 语法必须引用react
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// 把App组件挂载到id=root这个DOM上
+// JSX 语法
 ReactDOM.render(<App />, document.getElementById('root'));
